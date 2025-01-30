e197a2529e5873b70a9399867845cf274e16a1dc
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-30 19:25:51 +0300

    create user data and user posts

--- main.ts
+++ main.ts
@@ -1,15 +1,46 @@
 import { PrismaClient } from '@prisma/client'
+import { data } from 'cheerio/dist/commonjs/api/attributes'
+import { create } from 'domain'
+import { title } from 'process'
 
 const prisma = new PrismaClient()
 
 async function main() {
 
-    const user = await prisma.user.create({
-        data: {
-            name: "Emre Seferoğlu",
-            email: "emreseferoglu@mail.com"
-        }
-    })
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "Yunus emre seferoglu",
+    //         email: "seferogluemre@mail.com",
+
+    //         posts: {
+    //             create: [
+    //                 {
+    //                     title: "Emre Seferoglu kimdir",
+    //                     content: "Emre seferoglu İstanbulda dogdu"
+    //                 },
+    //                 {
+    //                     title: "OnlyJS Akademi",
+    //                 },
+    //                 {
+    //                     title: "Javascript Hangi alanlarda kullanılır",
+    //                     content: "Web geliştirme,Mobil Uygulama geliştirme,Sunucu geliştirme,Ön Uç geliştirme"
+    //                 },
+    //             ]
+    //         },
+    //     },
+    //     select: {
+    //         name: true,
+    //         posts: {
+    //             select: {
+    //                 title: true,
+    //             },
+
+    //         }
+
+    //     }
+    // })
+
+
     console.log(user)
 }
 
